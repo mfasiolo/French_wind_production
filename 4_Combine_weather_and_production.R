4fc5b0a2a1415b790793a37974d72c4649385537
Ben Griffiths <bengriffiths117@hotmail.co.uk>
2025-09-12 11:29:45 +0100

    Separate function to load region average from just region name.

--- 4_Combine_weather_and_production.R
+++ 4_Combine_weather_and_production.R
@@ -1,16 +1,24 @@
+library(readr)
+library(dplyr)
+
 source("R/combine_data_funcs.R")
 
-
-
-read_csv("production/wind_installed_capacity_2018-2023.csv",
+# Look at names of regions
+reg_names <- read_csv("production/wind_installed_capacity_2018-2023.csv",
          locale = locale(tz = "UTC")) |> 
   names()
 
+reg_names[-1]
 
-wind_dat <- read_csv("temp/PACA_wind_data.csv",
+# Pick a region, e.g. Grand Est
+region <- "Grand Est"
+
+# Use combine reg ave to load production and region averages for this region
+GE_dat <- load_region_ave(region)
+
+
+# Alternatively, if other summaries of regional data are to be combined,
+# can use combine_data directly (replace wind_dat with preferred data for region)
+wind_dat <- read_csv(paste0("Wind_data/Regional_wind_data/Grand.Est_wind_data.csv"),
                      locale = locale(tz = "UTC"))
-region <- "PACA"
-
-
-
-dat <- combine_data(region, wind_dat)
+GE_dat <- combine_data(region, wind_dat)
--- R/combine_data_funcs.R
+++ R/combine_data_funcs.R
@@ -1,22 +1,36 @@
 # function to combine weather data with production data in a given region
+# region should follow naming of columns in production
 combine_data <- function(region, weather_data) {
   
   # load capacity and filter by region
-  region_cap <- readr::read_csv("production/wind_installed_capacity_2018-2023.csv",
-                       locale = readr::locale(tz = "UTC")) |> 
-    dplyr::rename(datetime = Date) |> 
-    dplyr::select("datetime", paste0(region)) |> 
-    dplyr::rename(capacity = paste0(region))
+  region_cap <- read_csv("production/wind_installed_capacity_2018-2023.csv",
+                       locale = locale(tz = "UTC")) |> 
+    rename(datetime = Date) |> 
+    select("datetime", paste0(region)) |> 
+    rename(capacity = paste0(region))
   
   # load production, filter by region, and combine with weather and capacity
   # discarding missing values
-  region_dat <- readr::read_csv("production/wind_regional_production_2018-2023.csv",
-                       locale = readr::locale(tz = "UTC")) |> 
-    dplyr::rename(datetime = Date) |> 
-    dplyr::select("datetime", paste0(region)) |> 
-    dplyr::rename(production = paste0(region)) |> 
-    dplyr::inner_join(region_cap, by = "datetime") |> 
-    dplyr::inner_join(weather_data, by = "datetime")
+  region_dat <- read_csv("production/wind_regional_production_2018-2023.csv",
+                       locale = locale(tz = "UTC")) |> 
+    rename(datetime = Date) |> 
+    select("datetime", paste0(region)) |> 
+    rename(production = paste0(region)) |> 
+    inner_join(region_cap, by = "datetime") |> 
+    inner_join(weather_data, by = "datetime")
   
   return(region_dat)
+}
+
+
+
+
+# Function input region
+load_region_ave <- function(region) {
+  # load regional weather data
+  region_file_name <- gsub("-", ".", gsub(" ", ".", region))
+  wind_dat <- read_csv(paste0("Wind_data/Regional_wind_data/", region_file_name, "_wind_data.csv"),
+                       locale = locale(tz = "UTC"))
+  
+  combine_data(region, wind_dat)
 }
