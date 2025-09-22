f69c26a367a90ee4e0cbfe3e45afa01ad7c19dc8
Ben Griffiths <zp21899@it096506.wks.bris.ac.uk>
2025-09-22 10:55:13 +0100

    Move wind speed and direction to combine_data

--- 5_basic_gam.R
+++ 5_basic_gam.R
@@ -7,26 +7,12 @@ source("R/combine_data_funcs.R")
 
 GE_dat <- load_region_ave("Grand Est")
 
-GE_dat$prod_div_cap <- GE_dat$production / GE_dat$capacity
-
-GE_dat$time <- format(GE_dat$datetime, format = "%H:%M")
-GE_dat$toy <- yday(GE_dat$datetime)
-GE_dat$tod <- hour(GE_dat$datetime) + minute(GE_dat$datetime)/60
-GE_dat$trend <- as.numeric(difftime(GE_dat$datetime, GE_dat$datetime[1], units = "days"))
-
-
-GE_dat$windspeed10 <- sqrt(GE_dat$u10^2 + GE_dat$v10^2)
-GE_dat$windspeed100 <- sqrt(GE_dat$u100^2 + GE_dat$v100^2)
-
-GE_dat$direction10 <- atan2(GE_dat$u10, GE_dat$v10) + 180 / pi + 180
-GE_dat$direction100 <- atan2(GE_dat$u100, GE_dat$v100) + 180 / pi + 180
-
 names(GE_dat)
 
 form1 <- prod_div_cap ~ s(toy, bs = "cc", k = 5) + s(tod, bs = "cc", k = 10) +
-  s(capacity) + 
-  s(windspeed10) + s(direction10) + ti(windspeed10, direction10) + 
-  s(windspeed100) + s(direction100) + ti(windspeed100, direction100) + 
+  s(capacity) +
+  s(windspeed10) + s(direction10) + ti(windspeed10, direction10) +
+  s(windspeed100) + s(direction100) + ti(windspeed100, direction100) +
   ti(windspeed10, windspeed100)
 
 gam_fit1 <- bam(
@@ -55,26 +41,12 @@ GE_convex <- read_csv("temp/Grand.Est_convex.csv",
 
 GE_convex <- combine_data("Grand Est", GE_convex)
 
-GE_convex$prod_div_cap <- GE_convex$production / GE_convex$capacity
-
-
-GE_convex$time <- format(GE_convex$datetime, format = "%H:%M")
-GE_convex$toy <- yday(GE_convex$datetime)
-GE_convex$tod <- hour(GE_convex$datetime) + minute(GE_convex$datetime)/60
-GE_convex$trend <- as.numeric(difftime(GE_convex$datetime, GE_convex$datetime[1], units = "days"))
-
-
-GE_convex$windspeed10 <- sqrt(GE_convex$u10^2 + GE_convex$v10^2)
-GE_convex$windspeed100 <- sqrt(GE_convex$u100^2 + GE_convex$v100^2)
-
-GE_convex$direction10 <- atan2(GE_convex$u10, GE_convex$v10) + 180 / pi + 180
-GE_convex$direction100 <- atan2(GE_convex$u100, GE_convex$v100) + 180 / pi + 180
-
 
 gam_fit2 <- bam(
   form1,
   data = GE_convex,
   discrete = TRUE,
+  family = scat,
   argGam = list(knots=list(tod=seq(0,24,length=9)))
 ) |>
   getViz()
@@ -88,25 +60,13 @@ GE_concave <- read_csv("temp/Grand.Est_concave.csv",
 
 GE_concave <- combine_data("Grand Est", GE_concave)
 
-GE_concave$prod_div_cap <- GE_concave$production / GE_concave$capacity
-
-GE_concave$time <- format(GE_concave$datetime, format = "%H:%M")
-GE_concave$toy <- yday(GE_concave$datetime)
-GE_concave$tod <- hour(GE_concave$datetime) + minute(GE_concave$datetime)/60
-GE_concave$trend <- as.numeric(difftime(GE_concave$datetime, GE_concave$datetime[1], units = "days"))
-
-
-GE_concave$windspeed10 <- sqrt(GE_concave$u10^2 + GE_concave$v10^2)
-GE_concave$windspeed100 <- sqrt(GE_concave$u100^2 + GE_concave$v100^2)
-
-GE_concave$direction10 <- atan2(GE_concave$u10, GE_concave$v10) + 180 / pi + 180
-GE_concave$direction100 <- atan2(GE_concave$u100, GE_concave$v100) + 180 / pi + 180
 
 
 gam_fit3 <- bam(
   form1,
   data = GE_concave,
   discrete = TRUE,
+  family = scat,
   argGam = list(knots=list(tod=seq(0,24,length=9)))
 ) |>
   getViz()
--- R/combine_data_funcs.R
+++ R/combine_data_funcs.R
@@ -1,24 +1,38 @@
 # function to combine weather data with production data in a given region
 # region should follow naming of columns in production
 combine_data <- function(region, weather_data) {
-  
+
   # load capacity and filter by region
   region_cap <- read_csv("production/wind_installed_capacity_2018-2023.csv",
-                       locale = locale(tz = "UTC")) |> 
-    rename(datetime = Date) |> 
-    select("datetime", paste0(region)) |> 
+                       locale = locale(tz = "UTC")) |>
+    rename(datetime = Date) |>
+    select("datetime", paste0(region)) |>
     rename(capacity = paste0(region))
-  
+
   # load production, filter by region, and combine with weather and capacity
   # discarding missing values
   region_dat <- read_csv("production/wind_regional_production_2018-2023.csv",
-                       locale = locale(tz = "UTC")) |> 
-    rename(datetime = Date) |> 
-    select("datetime", paste0(region)) |> 
-    rename(production = paste0(region)) |> 
-    inner_join(region_cap, by = "datetime") |> 
+                       locale = locale(tz = "UTC")) |>
+    rename(datetime = Date) |>
+    select("datetime", paste0(region)) |>
+    rename(production = paste0(region)) |>
+    inner_join(region_cap, by = "datetime") |>
     inner_join(weather_data, by = "datetime")
-  
+
+  region_dat$prod_div_cap <- region_dat$production / region_dat$capacity
+
+  region_dat$time <- format(region_dat$datetime, format = "%H:%M")
+  region_dat$toy <- yday(region_dat$datetime)
+  region_dat$tod <- hour(region_dat$datetime) + minute(region_dat$datetime)/60
+  region_dat$trend <- as.numeric(difftime(region_dat$datetime, region_dat$datetime[1], units = "days"))
+
+
+  region_dat$windspeed10 <- sqrt(region_dat$u10^2 + region_dat$v10^2)
+  region_dat$windspeed100 <- sqrt(region_dat$u100^2 + region_dat$v100^2)
+
+  region_dat$direction10 <- atan2(region_dat$u10, region_dat$v10) * 180 / pi %% 360
+  region_dat$direction100 <- atan2(region_dat$u100, region_dat$v100) * 180 / pi %% 360
+
   return(region_dat)
 }
 
@@ -31,6 +45,6 @@ load_region_ave <- function(region) {
   region_file_name <- gsub("-", ".", gsub(" ", ".", region))
   wind_dat <- read_csv(paste0("Wind_data/Regional_wind_data/", region_file_name, "_wind_data.csv"),
                        locale = locale(tz = "UTC"))
-  
+
   combine_data(region, wind_dat)
 }
